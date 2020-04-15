f4cb8ac734d4f49509b8a44a4ccae4d569578996
LuisSky <luizguilhermeabrantes@gmail.com>
2020-04-14 23:50:30 -0300

    Added express and router structure

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const routes = require('./config/router');
+
+app.use(routes);
+
+app.use((req, res) => res.status(404).send('Page not found!'));
+
+app.listen(3000);
+
