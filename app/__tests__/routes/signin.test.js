61f4475963f103f4781c061bfcbcf5def21f64ad
LuisSky <luizguilhermeabrantes@gmail.com>
2020-04-19 21:47:03 -0300

    changed loginstyle

--- app/__tests__/routes/signin.test.js
+++ app/__tests__/routes/signin.test.js
@@ -3,9 +3,15 @@ const app = require('../../src/app');
 
 const MAIN_ROUTE = '/signin';
 
-test('Should response statuscode 200 in signin route', () => {
-  return supertest(app).get(MAIN_ROUTE)
-    .then((res) => {
-      expect(res.status).toBe(200);
-    });
+describe('Login Route', () => {
+
+  test('Should response statuscode 200 in signin route', () => {
+    return supertest(app).get(MAIN_ROUTE)
+      .then((res) => {
+        expect(res.status).toBe(200);
+      });
+  });
+
+
+
 });
